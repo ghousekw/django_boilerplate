406aaa3a2a761839027b20d40942670f50a60e34
gmshaik <gmshaik.kw@gmail.com>
2019-11-29 08:41:49 +0300

    Add django_boiler template

---
+++ src/settings/development.py
@@ -0,0 +1,28 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+
+def show_toolbar(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STRIPE_PUBLIC_KEY = ''
+STRIPE_SECRET_KEY = ''
